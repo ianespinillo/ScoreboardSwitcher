adbf5386db028d654192b56458fc87e7fdb07076
ianespinillo <ianespinillo@gmail.com>
2023-07-03 16:33:58 -0300

    0.3.5

---
+++ Script/carritoPopup.js
@@ -0,0 +1,5 @@
+const carritoImg= document.querySelector('.carrito-logo ');
+const carritoPopup= document.querySelector('#myModal');
+carritoImg.addEventListener('click', () => {
+    carritoPopup.style.display= 'block';
+});
